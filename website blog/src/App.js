44a9d8636193bc553367a850bd03973d889602c3
Hirwa-Joric <122998728+Hirwa-Joric@users.noreply.github.com>
2023-05-30 20:48:06 +0200

    updated

--- website blog/src/App.js
+++ website blog/src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import { AboutUs, ContactUs, Home, PopularBlog, PrivacyPolicy, Sidebar } from "./components/index"
-import {Route , Routes} from 'react-router-dom'
+import {Route , Router, Routes} from 'react-router-dom'
 
 
 
 function App() {
+  // return (
+  //     <div classNamte="App">
+  //     <AboutUs />
+  //     <ContactUs />
+  //     <Home />
+  //     <PopularBlog />
+  //     <PrivacyPolicy /> 
+  //     <Sidebar/>
+  //     </div>
+  // )
+  
   return (
-      <div classNamte="App">
-      <AboutUs />
-      <ContactUs />
-      <Home />
-      <PopularBlog />
-      <PrivacyPolicy /> 
-      <Sidebar/>
-      </div>
+    <>
+      <Routes>
+        <Route>
+          <Route/>
+        </Route>
+      </Routes>
+    </>
   )
 }
 export default App;
